c60c48867ec2e453f674bea95371838b7e141c3a
Nine <245328522@qq.com>
2017-03-18 22:26:23 +0800

    Initial Commit

---
+++ Square/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Square
+//
+//  Created by Nine on 2017/3/18.
+//  Copyright © 2017年 Nine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
